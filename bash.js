2c559c1aaf495e21904328300d803b3b508bfc04
Nataliia Riabkova <nataliiariabkova@gmail.com>
2020-03-09 13:46:33 -0400

    adds pwd command

--- bash.js
+++ bash.js
@@ -2,7 +2,23 @@ process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();
-    process.stdout.write('You typed: ' + cmd);
-    process.stdout.write('\nprompt > ');
+    if ( cmd === 'pwd'){
+        process.stdout.write(`Current directory: ${process.cwd()}`);
+    } else {
+        process.stdout.write('You typed: ' + cmd);
+    }
+    process.stdout.write('\nprompt > ')
 
 });
+
+// process.stdin.on('data', (data) => {
+
+//     const pwd = data.toString().trim();
+    
+//     process.stdout.write(`Current directory: ${process.cwd()}`);
+//     process.stdout.write('\nprompt > ');
+
+// });
+
+
+
