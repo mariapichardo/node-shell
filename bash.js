ace67e8db153cefaf8dba8067b9f518311e7d3b0
mariapichardo <maria_pichardo@yahoo.com>
2020-03-09 14:34:28 -0400

    give nataliia

--- bash.js
+++ bash.js
@@ -1,24 +1,18 @@
 process.stdout.write('prompt > ');
 
+const pwd = require('./pwd.js');
+const ls = require('./ls.js');
+
+
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();
-    if ( cmd === 'pwd'){
-        process.stdout.write(`Current directory: ${process.cwd()}`);
+    if (cmd === 'pwd') {
+        pwd();
+    } else if (cmd === 'ls') {
+        ls();
     } else {
         process.stdout.write('You typed: ' + cmd);
     }
     process.stdout.write('\nprompt > ')
 
 });
-
-// process.stdin.on('data', (data) => {
-
-//     const pwd = data.toString().trim();
-    
-//     process.stdout.write(`Current directory: ${process.cwd()}`);
-//     process.stdout.write('\nprompt > ');
-
-// });
-
-
-
